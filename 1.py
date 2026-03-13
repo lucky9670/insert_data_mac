0b125aff8b26bd609f35c9b0cb55ad4bdba230de
lucky9670 <riteshbharti262903@gmail.com>
2026-03-13 12:57:08 +0530

    Added next files to upload

--- 1.py
+++ 1.py
@@ -1,7 +1,11 @@
 from pathlib import Path
+import json
 
-folder_path = Path(r"clay_company/")
+folder_path = Path(r"clay_company_next/")
 
 files = [file.name for file in folder_path.iterdir() if file.is_file()]
+
+with open("files.json", "w") as json_file:
+    json.dump(files, json_file)
 
 print(files)
